1476786ebb7500c9dd7c680412daa815d6720764
NereuAugusto <nereubeckercampos711@gmail.com>
2021-11-10 10:55:39 -0300

    Ajustes

--- routes/index.ts
+++ routes/index.ts
@@ -2,8 +2,6 @@
 
 class IndexRoute {
 	public async index(req: app.Request, res: app.Response) {
-		let nomeDoUsuarioQueVeioDoBanco = "Rafael";
-
 		let lista: any[];
 
 		await app.sql.connect(async (sql) => {
@@ -23,29 +21,14 @@ class IndexRoute {
 
 
 	public async produtos(req: app.Request, res: app.Response) {
-		let produtoA = {
-			id: 1,
-			nome: "Produto A",
-			valor: 25
-		};
+		let lista: any[];
 
-		let produtoB = {
-			id: 2,
-			nome: "Produto B",
-			valor: 15
-		};
-
-		let produtoC = {
-			id: 3,
-			nome: "Produto C",
-			valor: 100
-		};
-
-		let produtosVindosDoBanco = [ produtoA, produtoB, produtoC ];
+		await app.sql.connect(async (sql) => {
+			lista = await sql.query("select id, nome, descricao, preco from produto order by preco asc");
+		});
 
 		let opcoes = {
-			titulo: "Listagem de Produtos",
-			produtos: produtosVindosDoBanco
+			produtos: lista
 		};
 
 		res.render("index/produtos", opcoes);
