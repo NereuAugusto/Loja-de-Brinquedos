9543b2b625ba37ac975f85d280f072715076a74a
ZOU YI LAI <zouyilai@gmail.com>
2021-11-12 13:00:03 -0300

    Criação: cadastro
    Cadastro conectado ao banco

--- routes/index.ts
+++ routes/index.ts
@@ -19,6 +19,49 @@ class IndexRoute {
 		res.render("index/cadastro");
 	}
 
+	@app.http.post()
+	public async criarProduto(req: app.Request, res: app.Response) {
+		// Os dados enviados via POST ficam dentro de req.body
+		let produto = req.body;
+
+		// É sempre muito importante validar os dados do lado do servidor,
+		// mesmo que eles tenham sido validados do lado do cliente!!!
+		if (!produto) {
+			res.status(400);
+			res.json("Dados inválidos");
+			return;
+		}
+
+		if (!produto.nome) {
+			res.status(400);
+			res.json("Nome inválido");
+			return;
+		}
+
+		if (!produto.descricao) {
+			res.status(400);
+			res.json("Descriçao inválida");
+			return;
+		}
+
+		produto.preco = parseFloat(produto.preco);
+		if (isNaN(produto.preco) || produto.preco <= 0) {
+			res.status(400);
+			res.json("Preço inválido");
+			return;
+		}
+
+		await app.sql.connect(async (sql) => {
+
+			// Todas os comandos SQL devem ser executados aqui dentro do app.sql.connect().
+
+			// As interrogações serão substituídas pelos valores passados ao final, na ordem passada.
+			await sql.query("INSERT INTO produto (nome, descricao, preco) VALUES (?, ?, ?)", [produto.nome, produto.descricao, produto.preco]);
+
+		});
+
+		res.json(true);
+	}
 
 	public async produtos(req: app.Request, res: app.Response) {
 		let lista: any[];
